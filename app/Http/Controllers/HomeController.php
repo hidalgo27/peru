06ae2fe2112e4a76b1706c9e2c578b3d8c30bfb6
Hidalgo <hidalgochpnce@gmail.com>
2017-11-02 16:06:28 -0500

    Se actualizo contenido

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
 
     public function tours()
     {
-        $paquete = TPaquete::with('paquetes_destinos', 'precio_paquetes')->where('estado', 0)->get();
+        $paquete = TPaquete::with('paquetes_destinos', 'precio_paquetes')->get();
         $categoria = TCategoria::get();
         $paquete_destinos = TPaqueteDestino::with('destinos')->get();
         return view('page.packages', ['paquete'=>$paquete, 'paquete_destinos'=>$paquete_destinos, 'categoria'=>$categoria]);
