a1109ba0728ed155820940f0240c400236ea2577
Hidalgo <hidalgochpnce@gmail.com>
2018-02-28 17:39:27 -0500

    Se agrego ibrerias seo

--- resources/views/page/tours.blade.php
+++ resources/views/page/tours.blade.php
@@ -86,6 +86,7 @@
 
     <section class="pb-5 bg-white">
         <div class="container">
+            
             <div class="row">
                 <div class="col">
                     <div id="tours" class="d-sm-block pt-5">
