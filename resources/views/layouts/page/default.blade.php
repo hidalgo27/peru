d5dacc7d3e37d64352314889569a145c17ce9edf
Hidalgo <hidalgochpnce@gmail.com>
2018-03-09 13:10:12 -0500

    Se creo precios incluidos con hoteles en itinerary

--- resources/views/layouts/page/default.blade.php
+++ resources/views/layouts/page/default.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="jwORR3CP8HncGeqMvD-edmeNZKnF1mbTojRwWhyXzZA" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{--<title>Agencia de Viajes en Peru | Tours Machu Picchu</title>--}}
     {!! SEOMeta::generate() !!}
